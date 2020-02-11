d687377ffec7841f1055af69208846cd9e79dae2
Max Weijers <maxweijers@hotmail.nl>
2020-02-11 15:29:48 +0100

    routing works

--- docs/js/app.js
+++ docs/js/app.js
@@ -10,10 +10,27 @@ const numberOfPages = 9;
 
 var randomNumber = 0;
 
-GetData();
+var firstTime = true;
 
-async function GetData(){
-    await fetch(urlBase + urlExtensionCategory + urlPageExtension + RandomAPIPage(numberOfPages))
+routie('overview');
+
+//switchPage();
+
+
+// routie({
+//     'devtools/:id': id => {
+//       console.log(id);
+//     },
+//     'details': () => {
+//       details();
+//     },
+//     'overview': () => {
+//       overview();
+//     },
+//   });
+
+async function getData(){
+    await fetch(urlBase + urlExtensionCategory + urlPageExtension + randomAPIPage(numberOfPages))
         .then((response) => {                
             //handle client error with fetch
             if(response.ok) {
@@ -22,32 +39,46 @@ async function GetData(){
                 return Promise.reject(response);
             }            })
         .then((myjson) => {
-            SetHTML(myjson);
-            console.log(myjson);
+            // setHTML(myjson);
+            // console.log(myjson);
+
+            routie({
+                'devtools/:id': id => {
+                  console.log(id);
+                },
+                'details': () => {
+                  details();
+                },
+                'overview': () => {
+                  overview(myjson);
+                },
+              });
         })
         .catch(function(err){
             console.warn("something went wrong. ", err);   
     });   
-}  
+}
 
-function SetHTML(json){
+getData();
+
+function setHTML(json){
     numbersUsedList = [];
 
     //filter through data
     for(var i = 0; i < numberOfPeople; i++){
         //select current p through index
-        var currentP = document.getElementById("p" + index);
+        var currentP = document.getElementById("a" + index);
 
         //get length since not every list is 10
         var length = json.results.length;
 
-        var currentRandom = GetRandomNumber(length);
+        var currentRandom = getRandomNumber(length);
 
         currentP.innerText = json.results[currentRandom].name;
 
-        //increase index to cycle through p's
+        //increase index to cycle through a's
         index++;
-        //reset index to make sure it used the right p
+        //reset index to make sure it used the right a
         if(index > numberOfPeople){
             index = 1;
         }
@@ -55,13 +86,13 @@ function SetHTML(json){
 }
 
 //get a random page from the API
-function RandomAPIPage(maxNumberPages){
+function randomAPIPage(maxNumberPages){
     var randomPageNumber = Math.floor((Math.random() * maxNumberPages) + 1);
 
     return randomPageNumber;
 }
 
-function GetRandomNumber(maxNumber){
+function getRandomNumber(maxNumber){
     //generate random number
     randomNumber = Math.floor((Math.random() * maxNumber));
 
@@ -70,7 +101,7 @@ function GetRandomNumber(maxNumber){
         //reset random number to be sure
         randomNumber = null;
 
-        GetRandomNumber(maxNumber);
+        getRandomNumber(maxNumber);
     }
     //add number to list to check
     numbersUsedList.push(randomNumber);
@@ -79,16 +110,27 @@ function GetRandomNumber(maxNumber){
 }
 
 //called when clicking on character name
-function Details(){
-    SwitchPage();
+function details(){
+    switchPage();
+    console.log("details");
 }
 
 //called when clicking back button in detail page
-function Overview(){
-    SwitchPage();
+function overview(json){
+    console.log(firstTime);
+    if(firstTime){
+        setHTML(json);
+        console.log("overview");
+        firstTime = false;
+    }else{
+        switchPage();
+        setHTML(json);
+        console.log("overview");
+    }
 }
 
-function SwitchPage(){
+function switchPage(){
+    console.log("AAHHH");
     const overview = document.getElementById("overview");
     const detail = document.getElementById("details");
     overview.classList.toggle("hidden");
